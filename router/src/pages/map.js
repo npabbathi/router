b463876ee0102e7d9f14ea9d8799ac2625bbbc6a
AidanNgo <aidanngo88@gmail.com>
2025-03-23 16:27:19 -0500

    added wall.js, which loads in the image of the wall that the user clicked on. Map page is acting weird now in attemptingting to make it responsive. I have no idea how to fix it right now ughhh

--- router/src/pages/map.js
+++ router/src/pages/map.js
@@ -1,4 +1,5 @@
-import gym_map from '../images/map1.png';
+import gym_map from '../images/map1.png'; //default map image for now.
+
 import React from 'react';
 import './map.css';
 import ImageMapper from "react-img-mapper";
@@ -12,31 +13,44 @@ const areas = [
         coords: [194, 351, 228, 323, 246, 334, 242, 344, 247, 355, 236, 390, 225, 402, 203, 386], 
         fillColor: "rgba(255, 0, 0, 0.70)", 
         strokeColor: "black",
-        link: "/wall"
+        link: "/wall",
+        image: "/images/map1-walls/fs1.png"
      }
-
-
-    // { name: "Wall2", shape: "circle", coords: [194, 3, 5], preFillColor: "red" }
 ];
 
 const ClimbingMap = () => {
     const navigate = useNavigate();
+    const [mapWidth, setMapWidth] = useState(window.innerWidth * 0.8)
+
+    const handleResize = () => {
+        setMapWidth(window.innerWidth * 0.8); // Update width on resize
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const handleAreaClick = (area) => {
         if (area.link) {
-            navigate(area.link); // Navigate to the page
+            navigate(`${area.link}?image=${encodeURIComponent(area.image)}`);
         }
     };
 
     return (
         <div>
             <h1 className="header">Select a Wall</h1>
+            <div className="header">note: select FS1 for now</div>   {/** delete later once everything is fleshed */}
             <div className="map-con">
                 <ImageMapper
                     src={gym_map}
                     name="climbing-map"
                     areas={areas}
                     onClick={(area) => handleAreaClick(area)}
+                    // imgWidth={930}
+                    responsive
+                    parentWidth={mapWidth}
+                    natural
                 />
             </div>
         </div>
--- router/src/pages/wall.js
+++ router/src/pages/wall.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import './map.css';
+import { useLocation } from 'react-router-dom';
+
 const Wall = () => {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const imageUrl = params.get("image");
+
     return (
         <div>
-            <h1>
-                Select a Route
-            </h1>
+            <h1 className="header">Select a Route</h1>
+
+            <div className="map-con">
+                {imageUrl && <img src={imageUrl} alt="Selected Wall" className="map-img" />}
+            </div>
+
         </div>
-    )
-}
+    );
+};
+
 export default Wall;
