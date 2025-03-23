9995ef48777be3c23ecceabe61f9329eee325c79
AidanNgo <aidanngo88@gmail.com>
2025-03-22 21:07:29 -0500

    created a bad 'map' page. I am having issues integrating react-img-mapper, but will figure it out soon

--- router/src/pages/map.js
+++ router/src/pages/map.js
@@ -1,2 +1,23 @@
-const Map = () => <h1>Map</h1>;
-export default Map;
+import gym_map from '../images/map1.png';
+import React from 'react';
+import './map.css';
+import ImageMapper from "react-img-mapper";
+
+
+const ClimbingMap = () => {
+    return (
+        <div>
+            <h1 className="header">Select a Wall</h1>
+            <div className="map-con">
+                <img
+                    alt = ""
+                    className = "map-img"
+                    src={gym_map}
+                />
+            </div>
+
+        </div>
+    );
+};
+
+export default ClimbingMap;
