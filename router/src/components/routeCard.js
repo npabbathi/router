68549f7a48b3d2892c399ca603b725d25dda69b6
Nidhi Pabbathi <nidhi.pabbathi@utexas.edu>
2025-04-15 23:33:19 -0500

    trying to make drafts pretty but im eepy

--- router/src/components/routeCard.js
+++ router/src/components/routeCard.js
@@ -11,7 +11,7 @@ export const RouteCard = ({ name, grade, incline, description, onDeleteRoute, on
     const navigate = useNavigate();
 
     return (
-        <div className="routeCard">
+        <div className="routeCard" onClick={() => { can_modify ? onEditRoute(id, image) : navigate(`/review/${id}`) }}>
             {/* for icon buttons (credit below) */}
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <div className={`imagePreview ${isHovering ? "hover-cursor" : ""}`} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
@@ -19,17 +19,16 @@ export const RouteCard = ({ name, grade, incline, description, onDeleteRoute, on
                     <img src={image} alt={name}></img>
                 </div>
                 {isHovering &&
-                    <div className="overText" onClick={() => { can_modify ? onEditRoute(id, image) : navigate(`/review/${id}`) }}>
+                    <div className="overTextDrafts">
+                        <p> {description} </p>
                         <p3> {timestamp} </p3>
-                        <p> Description: {description}</p>
-                        <p> Notes: {notes} </p>
                     </div>
                 }
             </div>
             <div className="titleInfo">
                 <h1>{name}</h1>
-                {can_modify && <button onClick={() => { onEditRoute(id, image) }}><i class="fa fa-pencil"></i></button>}
-                <button onClick={() => { onDeleteRoute(id) }}><i class="fa fa-trash"></i></button>
+                {can_modify && <button className="editButton" onClick={() => { onEditRoute(id, image) }}><i class="fa fa-pencil"></i></button>}
+                <button className="trashButton" onClick={() => { onDeleteRoute(id) }}><i class="fa fa-trash"></i></button>
             </div>
             <h4>{grade} - {incline}º</h4>
         </div>
